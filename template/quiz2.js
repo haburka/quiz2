89e89ef07d221620100f148a201629938b2db4f5
Robert Webb <robwebb@bitbucket.org>
2016-03-28 19:35:22 -0400

    fixed basic errors

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,39 +1,32 @@
-(function($){
+$(function(){
 	// This is where you will write your function for the programming challenge
 	// Do not commit console.log statements
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$mouseover = $('.mouse-over');
+	$click     = $('.click');
+	$submit       = $('.submit');
 
-	$mouseover.on('mouseover', function() {
-		$this = $(this);
+	$mouseover.mouseover( function() {
 		$(this).html('Scrooge McDuck!');
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	$click.click(function() {
+		$(this).html('Peace Out!');
+		$(this).fadeOut(1500);
 		return false;
 	});
 
-	$submit.on('submit', function(e) {
+	$submit.click( function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
-			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+            $('#form').fadeOut('slow');
+			$(this).parent().append("<h2>Congratulations! You've entered some text!</h2>");
 		}
 	});
+    setTimeout(function(){
+        $('.timeout').fadeIn('slow');}, 1000);
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
-		}, 1000);
-	});
-
-})(jQuery);
+});
